20e338b759cc1f88b031f9c6ab150e8021ddccd7
bill <you@example.com>
2020-12-24 15:10:23 +0900

    useAsync 커스텀 훅 개발하여 적용

---
+++ src/User.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import useAsync from "./useAsync";
+
+async function getUser(id) {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/users/${id}`
+  );
+  return res.data;
+}
+
+
+function User({ id }) {
+  const [state] = useAsync(() => getUser(id), [id]);
+  const { loading, data: user, error } = state;
+
+  if (loading) return <div>로딩중 </div>;
+  if (error) return <div>에러가 발생헀습니다. </div>;
+  if (!user) return null;
+
+  return <div>
+      <h2>{user.username}</h2>
+  </div>;
+}
+
+export default User;
--- src/Users.js
+++ src/Users.js
@@ -1,49 +1,34 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { asyncReducer } from "./asyncReducer";
+import useAsync from "./useAsync";
+import User from "./User";
+
+async function getUsers() {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users/");
+  return res.data;
+}
 
 function Users() {
-  const [state, dispatch] = useReducer(asyncReducer, {
-    loading: false,
-    data: null,
-    error: null,
-  });
-
-  const fetchUses = async () => {
-    dispatch({type:'LOADING'});
-    try {
-      const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/users/"
-      );
-      dispatch({type:'SUCCESS',data:res.data});
-    } catch (e) {
-      dispatch({type:'ERROR',error:e});
-    }
-
-    
-  };
-
-  useEffect(() => {
-    fetchUses();
-  }, []);
-
+  const [state, refetch] = useAsync(getUsers, [], true);
   const { loading, data: users, error } = state;
+  const [userId, setUserId] = useState(null);
 
   if (loading) return <div>로딩중 </div>;
   if (error) return <div>에러가 발생헀습니다. </div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>요청하기 </button>;
 
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={()=>setUserId(user.id)}>
             {user.username}({user.name})
           </li>
         ))}
         <li></li>
       </ul>
-      <button onClick={fetchUses}>다시 요청하기 </button>
+      <button onClick={refetch}>다시 요청하기 </button>
+      {userId && <User id={userId}/>}
     </>
   );
 }
